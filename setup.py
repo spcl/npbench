1c52cd7b5493c72b870289415e1e9b44a875a78f
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2021-04-28 19:44:54 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='dace',
+      version='0.1',
+      url='https://https://github.com/spcl/npbench',
+      author='SPCL @ ETH Zurich',
+      author_email='alziogas@inf.ethz.ch',
+      description='NPBench',
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
+      install_requires=[ 'numpy', "pygount", "sqlite3"])
