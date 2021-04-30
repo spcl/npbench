8ba9c7653191b47fb7bda3f54bae2abae0a23e9b
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2021-04-30 17:35:37 +0200

    Added script for running all benchmarks with a specific framework.

--- run_framework.py
+++ run_framework.py
@@ -1,15 +1,12 @@
 import argparse
+import os
+import pathlib
 
 from npbench.infrastructure import (Benchmark, generate_framework, LineCount,
                                     Test, utilities as util)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b",
-                        "--benchmark",
-                        type=str,
-                        nargs="?",
-                        required=True)
     parser.add_argument("-f",
                         "--framework",
                         type=str,
@@ -36,10 +33,20 @@ if __name__ == "__main__":
 
     print(args)
 
-    bench = Benchmark(args["benchmark"])
     frmwrk = generate_framework(args["framework"])
     numpy = generate_framework("numpy")
-    lcount = LineCount(bench, frmwrk, numpy)
-    lcount.count()
-    test = Test(bench, frmwrk, numpy)
-    test.run(args["preset"], args["validate"], args["repeat"], args["timeout"])
+
+    parent_folder = pathlib.Path(__file__).parent.absolute()
+    bench_dir = parent_folder.joinpath("bench_info")
+    pathlist = pathlib.Path(bench_dir).rglob('*.json')
+    for path in pathlist:
+        benchname = os.path.basename(path)[:-5]
+        bench = Benchmark(benchname)
+        lcount = LineCount(bench, frmwrk, numpy)
+        lcount.count()
+        test = Test(bench, frmwrk, numpy)
+        try:
+            test.run(args["preset"], args["validate"], args["repeat"],
+                     args["timeout"])
+        except:
+            continue
