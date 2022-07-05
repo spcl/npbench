f2b86cad5dda0f87376d99ba28047f3204c51f70
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2022-07-05 17:34:40 +0200

    validated examples

--- npbench/benchmarks/azimint_naive/azimint_naive_mpi4py.py
+++ npbench/benchmarks/azimint_naive/azimint_naive_mpi4py.py
@@ -49,15 +49,16 @@ def azimint_naive_easy(data, radius, npt):
     return res
 
 
-def azimint_naive_hard(data, radius, npt, N):
+def azimint_naive_hard(data, radius, npt):
     """ Distributes by `N`. `data` and `radius` are distributed accordingly. """
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    rmax = radius.max()  # Local reduction
-    comm.AllReduce(MPI.IN_PLACE, rmax, MPI.MAX)  # Global reduction
+    rmax = np.empty((1,), dtype=radius.dtype)
+    rmax[0] = radius.max()  # Local reduction
+    comm.Allreduce(MPI.IN_PLACE, rmax, op=MPI.MAX)  # Global reduction
     res = np.zeros(npt, dtype=np.float64)
     for i in range(npt):
         r1 = rmax * i / npt
@@ -65,9 +66,37 @@ def azimint_naive_hard(data, radius, npt, N):
         mask_r12 = np.logical_and((r1 <= radius), (radius < r2))
         values_r12 = data[mask_r12]
         # res[i] = values_r12.mean()
-        num = values_r12.shape[0]
-        comm.AllReduce(MPI.IN_PLACE, num, MPI.SUM)
-        res[i] = values_r12.sum()
-        comm.AllReduce(MPI.IN_PLACE, res[i], MPI.SUM)
-        res[i] /= num
+        num = np.empty((1,), dtype=np.int32)
+        num[0] = values_r12.shape[0]
+        comm.Allreduce(MPI.IN_PLACE, num, MPI.SUM)
+        tmp = np.empty((1,), dtype=values_r12.dtype)
+        tmp[0] = values_r12.sum()
+        comm.Allreduce(MPI.IN_PLACE, tmp, MPI.SUM)
+        res[i] = tmp[0] / num
     return res
+
+
+if __name__ == "__main__":
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    N = 1000000
+    npt = 10
+    from azimint_naive import initialize
+    data, radius = initialize(N)
+
+    ref_res = azimint_naive(data, radius, npt)
+
+    start = rank * int(np.ceil(npt / size))
+    finish = min(npt, (rank + 1) * int(np.ceil(npt / size)))
+    easy_res = azimint_naive_easy(data, radius, npt)
+    if not (easy_res is None):
+        assert(np.allclose(easy_res, ref_res[start:finish]))
+    
+    start = rank * int(np.ceil(N / size))
+    finish = min(N, (rank + 1) * int(np.ceil(N / size)))
+    if finish - start > 0:
+        hard_res = azimint_naive_hard(data[start:finish], radius[start:finish], npt)
+        assert(np.allclose(hard_res, ref_res))
--- npbench/benchmarks/go_fast/go_fast_mpi4py.py
+++ npbench/benchmarks/go_fast/go_fast_mpi4py.py
@@ -15,13 +15,11 @@ def go_fast_single(a, first_row):
     """ Distributes by `N`. By convention distributed by rows. """
 
     comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
 
-    trace = 0.0
+    trace = np.zeros((1,), dtype=a.dtype)
     for i in range(a.shape[0]):
-        trace += np.tanh(a[i, first_row + i])
-    comm.AllReduce(MPI.IN_PLACE, trace, MPI.SUM)
+        trace[0] += np.tanh(a[i, first_row + i])
+    comm.Allreduce(MPI.IN_PLACE, trace, MPI.SUM)
     return a + trace
 
 
@@ -29,14 +27,46 @@ def go_fast_double(a, first_row, first_col):
     """ Distributes by `NxN`. """
 
     comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
 
-    trace = 0.0
+    trace = np.zeros((1,), dtype=a.dtype)
     for i in range(a.shape[0]):
         global_col = first_row + i
         local_col = global_col - first_col
         if local_col >= 0 and local_col < a.shape[1]:
-            trace += np.tanh(a[i, local_col])
-    comm.AllReduce(MPI.IN_PLACE, trace, MPI.SUM)
+            trace[0] += np.tanh(a[i, local_col])
+    comm.Allreduce(MPI.IN_PLACE, trace, MPI.SUM)
     return a + trace
+
+
+if __name__ == "__main__":
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    N = 1000
+    from go_fast import initialize
+    a = initialize(N)
+
+    ref = go_fast(a)
+
+    start = rank * int(np.ceil(N / size))
+    finish = min(N, (rank + 1) * int(np.ceil(N / size)))
+    if finish - start > 0:
+        single = go_fast_single(a[start:finish], start)
+        assert(np.allclose(single, ref[start:finish]))
+    
+
+    Px = 2
+    Py = size // 2
+    P = Px * Py
+    comm2 = comm.Create_cart([Px, Py])
+    if rank < P:
+        coords = comm2.Get_coords(rank)
+        srow = coords[0] * int(np.ceil(N / Px))
+        frow = min(N, (coords[0] + 1) * int(np.ceil(N / Px)))
+        scol = coords[1] * int(np.ceil(N / Py))
+        fcol = min(N, (coords[1] + 1) * int(np.ceil(N / Py)))
+        if frow - srow > 0 and fcol - scol > 0:
+            double = go_fast_double(a[srow:frow, scol:fcol], srow, scol)
+            assert(np.allclose(double, ref[srow:frow, scol:fcol]))
