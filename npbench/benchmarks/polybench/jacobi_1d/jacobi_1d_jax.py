cc88ecd745c11c71a1f2602d7755727a2f361247
hardik01shah <hardik01shah@gmail.com>
2024-11-03 12:16:23 +0100

    Update jacobi_1d jax implementation

--- npbench/benchmarks/polybench/jacobi_1d/jacobi_1d_jax.py
+++ npbench/benchmarks/polybench/jacobi_1d/jacobi_1d_jax.py
@@ -1,12 +1,19 @@
 import jax
 import jax.numpy as jnp
+from jax import lax
 from functools import partial
 
 @partial(jax.jit, static_argnums=(0,))
 def kernel(TSTEPS: int, A: jax.Array, B: jax.Array):
 
-    for t in range(1, TSTEPS):
+    def body_fn(t, arrays):
+        A, B = arrays
+        # Update B based on A, and then update A based on the new B
         B = B.at[1:-1].set(0.33333 * (A[:-2] + A[1:-1] + A[2:]))
         A = A.at[1:-1].set(0.33333 * (B[:-2] + B[1:-1] + B[2:]))
+        return A, B
+
+    # Run the loop for TSTEPS iterations
+    A, B = lax.fori_loop(1, TSTEPS, body_fn, (A, B))
 
     return A, B
