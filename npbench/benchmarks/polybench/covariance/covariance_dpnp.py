5aba2d535c36ab4c096300856bbe54f2c636d0f1
di52vum <Salvatore.Cielo@lrz.de>
2024-11-11 15:19:29 +0100

    DPNP Covariance is just a numpy call. If not accepted, we will remove the bm from our list. Source yet to be cleaned otherwise.

--- npbench/benchmarks/polybench/covariance/covariance_dpnp.py
+++ npbench/benchmarks/polybench/covariance/covariance_dpnp.py
@@ -1,10 +1,10 @@
 import dpnp as np
 
 def kernel(M, float_n, data):
-    mean = np.mean(data, axis=0)
-    data -= mean
-    cov = np.zeros((M, M), dtype=data.dtype)
-
-    for i in range(M):
-        cov[i:M, i] = cov[i, i:M] = np.dot(data[:, i], data[:, i:M]) / (float_n - 1.0)
-    return cov
+#    mean = np.mean(data, axis=0)
+#    data -= mean
+##    cov = np.zeros((M, M), dtype=data.dtype)
+#    cov = np.dot(np.transpose(data),data) /(float_n-1.0)
+##    for i in range(M):
+##        cov[i:M, i] = cov[i, i:M] = np.dot(data[:, i], data[:, i:M]) / (float_n - 1.0)
+    return np.cov(np.transpose(data))
