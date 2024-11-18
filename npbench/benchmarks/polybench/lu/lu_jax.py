e6b37d17b8fa3dad0e3553478a5d6c8dc8245fe0
Sushant S <sushantswamy1212@gmail.com>
2024-11-18 07:19:04 +0100

    update lu

--- npbench/benchmarks/polybench/lu/lu_jax.py
+++ npbench/benchmarks/polybench/lu/lu_jax.py
@@ -1,15 +1,31 @@
 import jax
 import jax.numpy as jnp
+from jax import lax
 
 
 @jax.jit
 def kernel(A):
-    
-    for i in range(A.shape[0]):
-        for j in range(i):
-            A = A.at[i, j].set(A[i, j] - A[i, :j] @ A[:j, j])
-            A = A.at[i, j].set(A[i, j] / A[j, j])
-        for j in range(i, A.shape[0]):
-            A = A.at[i, j].set(A[i, j] - A[i, :i] @ A[:i, j])
 
+    def loop_body(i, A):
+
+        def inner_loop_1(j, A):
+            A_slice_1 = jnp.where(jnp.arange(A.shape[1]) < j, A[i, :], 0.0)
+            A_slice_2 = jnp.where(jnp.arange(A.shape[0]) < j, A[:, j], 0.0)
+            A = A.at[i, j].set(A[i, j] - A_slice_1 @ A_slice_2)
+            A = A.at[i, j].set(A[i, j] / A[j, j])
+            return A
+        
+        def inner_loop_2(j, A):
+            A_slice_1 = jnp.where(jnp.arange(A.shape[1]) < i, A[i, :], 0.0)
+            A_slice_2 = jnp.where(jnp.arange(A.shape[0]) < i, A[:, j], 0.0)
+            A = A.at[i, j].set(A[i, j] - A_slice_1 @ A_slice_2)
+            return A
+        
+        A = lax.fori_loop(0, i, inner_loop_1, A)
+        A = lax.fori_loop(i, A.shape[0], inner_loop_2, A)
+
+        return A
+    
+    A = lax.fori_loop(0, A.shape[0], loop_body, A)
+    
     return A
