bb8661dcbb2981402c0bbc6da0e1aaf139c4c9bf
Sushant S <sushantswamy1212@gmail.com>
2024-11-18 07:23:19 +0100

    update trisolv

--- npbench/benchmarks/polybench/trisolv/trisolv_jax.py
+++ npbench/benchmarks/polybench/trisolv/trisolv_jax.py
@@ -5,7 +5,13 @@ import jax.numpy as jnp
 @jax.jit
 def kernel(L, x, b):
 
-    for i in range(x.shape[0]):
-        x = x.at[i].set((b[i] - L[i, :i] @ x[:i]) / L[i, i])
+    def loop_body(i, loop_vars):
+        L, x, b = loop_vars
+        L_slice = jnp.where(jnp.arange(L.shape[1]) < i, L[i, :], 0.0)
+        x_slice = jnp.where(jnp.arange(x.shape[0]) < i, x, 0.0)
+        x = x.at[i].set((b[i] - L_slice @ x_slice) / L[i, i])
+        return L, x, b
+
+    _, x, _ = jax.lax.fori_loop(0, x.shape[0], loop_body, (L, x, b))
 
     return x
