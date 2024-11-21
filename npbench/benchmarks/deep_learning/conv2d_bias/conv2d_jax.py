bbc546e41088e9051e980ffd2e5d78587ef8fc65
hardik01shah <hardik01shah@gmail.com>
2024-11-21 11:49:52 +0100

    Update conv2d_jax with lax.scan

--- npbench/benchmarks/deep_learning/conv2d_bias/conv2d_jax.py
+++ npbench/benchmarks/deep_learning/conv2d_bias/conv2d_jax.py
@@ -12,33 +12,29 @@ def conv2d(input, weights):
     W_out = input.shape[2] - K + 1
     C_out = weights.shape[3]
     output = jnp.empty((N, H_out, W_out, C_out), dtype=jnp.float32)
-    
-    def col_update(j, arrays):
-        input, weights, output, i = arrays
 
-        input_slice = lax.dynamic_slice(
-            input, 
-            (0, i, j, 0), 
-            (N, K, K, input.shape[-1])
-        )
-        conv_result = jnp.sum(
-            input_slice[:, :, :, :, jnp.newaxis] * weights[jnp.newaxis, :, :, :], 
-            axis=(1, 2, 3)
-        )
-        output = lax.dynamic_update_slice(
-            output, 
-            conv_result[:, jnp.newaxis, jnp.newaxis, :], 
-            (0, i, j, 0)
-        )
-        return input, weights, output, i
+    def row_update(output, i):
+        def col_update(output, j):
+            input_slice = lax.dynamic_slice(
+                input, 
+                (0, i, j, 0), 
+                (N, K, K, input.shape[-1])
+            )
+            conv_result = jnp.sum(
+                input_slice[:, :, :, :, None] * weights[None, :, :, :], 
+                axis=(1, 2, 3)
+            )
+            output = lax.dynamic_update_slice(
+                output, 
+                conv_result[:, None, None, :], 
+                (0, i, j, 0)
+            )
+            return output, None
 
-    def row_update(i, arrays):
-        input, weights, output = arrays
-        arrays = (input, weights, output, i)
-        _, _, output, _ = lax.fori_loop(0, W_out, col_update, arrays)
-        return input, weights, output
+        output, _ = lax.scan(col_update, output, jnp.arange(W_out))
+        return output, None
 
-    _, _, output = lax.fori_loop(0, H_out, row_update, (input, weights, output))
+    output, _ = lax.scan(row_update, output, jnp.arange(H_out))
 
     return output
 
