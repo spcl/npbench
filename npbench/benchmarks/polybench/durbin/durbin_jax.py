731f0d872e8b5056db42736f6eae6d3f4b228d37
Sushant S <sushantswamy1212@gmail.com>
2024-11-21 08:38:53 +0100

    update durbin

--- npbench/benchmarks/polybench/durbin/durbin_jax.py
+++ npbench/benchmarks/polybench/durbin/durbin_jax.py
@@ -1,5 +1,6 @@
 import jax
 import jax.numpy as jnp
+from jax import lax
 
 
 @jax.jit
@@ -10,10 +11,21 @@ def kernel(r):
     beta = 1.0
     y = y.at[0].set(-r[0])
 
-    for k in range(1, r.shape[0]):
+
+    def loop_body(k, loop_vars):
+        alpha, beta, y, r = loop_vars
         beta *= 1.0 - alpha * alpha
-        alpha = -(r[k] + jnp.dot(jnp.flip(r[:k]), y[:k])) / beta
-        y = y.at[:k].add(alpha * jnp.flip(y[:k]))
+        
+        r_slice = jnp.where(jnp.arange(r.shape[0]) < k, jnp.roll(jnp.flip(r), [k], 0), 0.0)
+        y_slice = jnp.where(jnp.arange(y.shape[0]) < k, y, 0.0)
+        alpha = -(r[k] + jnp.dot(r_slice, y_slice)) / beta
+
+        y_update_slice = jnp.where(jnp.arange(y.shape[0]) < k, jnp.roll(jnp.flip(y), [k], 0) * alpha, 0.0)
+        y += y_update_slice
         y = y.at[k].set(alpha)
+
+        return alpha, beta, y, r
+
+    _, _, y, _ = lax.fori_loop(1, r.shape[0], loop_body, (alpha, beta, y, r))
 
     return y
