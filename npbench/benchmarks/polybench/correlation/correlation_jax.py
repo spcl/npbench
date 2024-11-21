091b29579c70fa0b859d881c328ae9273d1a4ef3
Sushant S <sushantswamy1212@gmail.com>
2024-11-21 09:28:13 +0100

    update_correlation

--- npbench/benchmarks/polybench/correlation/correlation_jax.py
+++ npbench/benchmarks/polybench/correlation/correlation_jax.py
@@ -6,6 +6,14 @@ from functools import partial
 @partial(jax.jit, static_argnums=(0,))
 def kernel(M, float_n, data):
 
+
+    def loop_body(i, loop_vars):
+        corr, data = loop_vars
+        corr = lax.dynamic_update_slice(corr, jnp.roll(data[i, None], -(i + 1), axis=1), (i, i + 1))
+        corr = lax.dynamic_update_slice(corr, jnp.roll(data[i, None], -(i + 1), axis=1).T, (i + 1, i))
+
+        return corr, data
+
     mean = jnp.mean(data, axis=0)
     stddev = jnp.std(data, axis=0)
     stddev = jnp.where(stddev <= 0.1, 1.0, stddev)
@@ -13,8 +21,8 @@ def kernel(M, float_n, data):
     data = data / (jnp.sqrt(float_n) * stddev)
     corr = jnp.eye(M, dtype=data.dtype)
 
-    for i in range(M - 1):
-        corr = corr.at[i + 1:M, i].set(data[:, i] @ data[:, i + 1:M]) 
-        corr = corr.at[i, i + 1:M].set(data[:, i] @ data[:, i + 1:M])
+    data_mul = jnp.dot(data.T, data)
+
+    corr, _ = lax.fori_loop(0, M - 1, loop_body, (corr, data_mul))
 
     return corr
