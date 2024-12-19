1dd397756438edc77edccee76b261ae733b1958c
Filip Jaksic <jaksicf@ethz.ch>
2024-12-19 17:55:14 +0100

    Make lu go brrr
    Previously was up to 70x slower, now it's faster for smaller sizes and comparable to numpy for bigger ones.

--- npbench/benchmarks/polybench/lu/lu_jax.py
+++ npbench/benchmarks/polybench/lu/lu_jax.py
@@ -7,17 +7,18 @@ from jax import lax
 def kernel(A):
 
     def loop_body(i, A):
-
         def inner_loop_1(j, A):
-            A_slice_1 = jnp.where(jnp.arange(A.shape[1]) < j, A[i, :], 0.0)
-            A_slice_2 = jnp.where(jnp.arange(A.shape[0]) < j, A[:, j], 0.0)
-            A = A.at[i, j].set(A[i, j] - A_slice_1 @ A_slice_2)
-            A = A.at[i, j].set(A[i, j] / A[j, j])
+            mask = jnp.arange(A.shape[0]) < j
+            A_slice_1 = jnp.where(mask, A[i, :], 0.0)
+            A_slice_2 = jnp.where(mask, A[:, j], 0.0)
+
+            A = A.at[i, j].set((A[i, j] - A_slice_1 @ A_slice_2) / A[j, j])
             return A
         
         def inner_loop_2(j, A):
-            A_slice_1 = jnp.where(jnp.arange(A.shape[1]) < i, A[i, :], 0.0)
-            A_slice_2 = jnp.where(jnp.arange(A.shape[0]) < i, A[:, j], 0.0)
+            mask = jnp.arange(A.shape[0]) < i
+            A_slice_1 = jnp.where(mask, A[i, :], 0.0)
+            A_slice_2 = jnp.where(mask, A[:, j], 0.0)
             A = A.at[i, j].set(A[i, j] - A_slice_1 @ A_slice_2)
             return A
         
