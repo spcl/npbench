5d2d00fda4d9c8a9ae5c0051e97a8789fdd8141f
hardik01shah <hardik01shah@gmail.com>
2024-12-20 00:25:31 +0100

    Update cholesky jax implementation

--- npbench/benchmarks/polybench/cholesky/cholesky_jax.py
+++ npbench/benchmarks/polybench/cholesky/cholesky_jax.py
@@ -11,17 +11,19 @@ def kernel(A):
 
         def col_update(j, A):
             mask = jnp.arange(A.shape[1]) < j
-            products = jnp.where(mask, A[i, :] * A[j, :], 0.0)
-            dot_prod = jnp.sum(products)
-            A = A.at[i, j].set((A[i, j] - dot_prod) / A[j, j])
+
+            A_i_slice = jnp.where(mask, A[i, :], 0)
+            A_j_slice = jnp.where(mask, A[j, :], 0)
+
+            dot_product = jnp.dot(A_i_slice, A_j_slice)
+            A = A.at[i, j].set((A[i, j] - dot_product) / A[j, j])
 
             return A
 
         A = lax.fori_loop(0, i, col_update, A)
 
-        mask = jnp.arange(A.shape[1]) < i
-        products = jnp.where(mask, A[i, :] * A[i, :], 0)
-        dot_product = jnp.sum(products)
+        A_i_slice = jnp.where(jnp.arange(A.shape[1]) < i, A[i, :], 0)
+        dot_product = jnp.dot(A_i_slice, A_i_slice)
         A = A.at[i, i].set(jnp.sqrt(A[i, i] - dot_product))
 
         return A
