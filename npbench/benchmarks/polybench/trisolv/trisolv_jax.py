58f2cdb995ee5957a25b1d74b7875a603d6af630
Sushant S <sushantswamy1212@gmail.com>
2024-12-20 12:23:03 +0100

    fix trisolv

--- npbench/benchmarks/polybench/trisolv/trisolv_jax.py
+++ npbench/benchmarks/polybench/trisolv/trisolv_jax.py
@@ -11,7 +11,7 @@ def kernel(L, x, b):
         mask = jnp.arange(x.shape[0]) < i
         products = jnp.where(mask, L[i, :] * x, 0.0) 
         dot_product = jnp.sum(products)
-        x.at[i].set((b[i] - dot_product) / L[i, i])
+        x = x.at[i].set((b[i] - dot_product) / L[i, i])
         return L, x, b
 
     _, x, _ = lax.fori_loop(0, x.shape[0], loop_body, (L, x, b))
